79687c7c2d3c672fdbf2bc9e3f4a70ce1f3b0ee2
Lorenzo <134946910+lorenzobruno7@users.noreply.github.com>
2024-05-22 11:55:56 +0200

    Add files via upload

---
+++ Esercizi/provaCompilazione.asm
@@ -0,0 +1,24 @@
+//File di prova per la compilazione
+asm provaCompilazione
+
+import ../Library/StandardLibrary
+
+signature:
+
+dynamic controlled variabileTest : Integer
+
+definitions:
+
+
+//CTL: Specificate tramite CTLSPEC
+
+
+//Invarianti (Sempre prima del main)
+
+main rule r_Main =
+	par
+		skip
+		skip
+	endpar
+
+default init s0:
