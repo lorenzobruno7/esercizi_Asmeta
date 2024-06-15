3823faaddde59301fa18baa819eeb35adce723f7
Lorenzo <134946910+lorenzobruno7@users.noreply.github.com>
2024-06-15 14:05:58 +0200

    Update playerToken.asm

--- Esercizi/esame250217_Token/playerToken.asm
+++ Esercizi/esame250217_Token/playerToken.asm
@@ -5,7 +5,7 @@ import ../../Library/CTLlibrary
 
 /*
  ----Modello AsmetaL - 25 maggio 2017----
----Esame riuscito in data 31 maggio 2024 (Con 4 Player e verifica del primo e ultimo giocatore che termina i token)---
+---Esame riuscito in data 31 maggio 2024 (Con 5 Player e verifica del primo e ultimo giocatore che termina i token per prendere 30)---
 Tre giocatori attorno ad un tavolo. Inizialmente ogni giocatore ha 3 token. Di seguito la rappresentazione grafica:
 
 ******************************* 	
